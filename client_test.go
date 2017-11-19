55689e411725fa9e234c930aba15e35bd601a540
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2017-11-19 17:33:25 +0900

    Add TestNewClient

--- client_test.go
+++ client_test.go
@@ -6,6 +6,11 @@ func TestNewClient(t *testing.T) {
 	token := "accesstoken"
 	client := NewClient(token)
 	if client.token != token {
-		t.Errorf("got %v, want %v", client.token, token)
+		t.Errorf("expected %v, got %v", token, client.token)
+	}
+
+	baseURL := "https://suzuri.jp"
+	if client.url.String() != baseURL {
+		t.Errorf("expected %v, got %v", baseURL, client.url)
 	}
 }
