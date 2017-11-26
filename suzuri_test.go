71948e612afb2e3212430a2647a6e877dc9f7d6e
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2017-11-26 13:14:51 +0900

    Add test setup, teardown

--- suzuri_test.go
+++ suzuri_test.go
@@ -1,9 +1,28 @@
 package suzuri
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 )
+
+var (
+	stub   *http.ServeMux
+	server *httptest.Server
+	ctx    context.Context
+	client *Client
+)
+
+func setup() {
+	stub, server = stubServer()
+	client = NewClient("accesstoken")
+	client.SetBaseURL(server.URL)
+	ctx = context.Background()
+}
+
+func teardown() {
+	server.Close()
+}
 
 func stubServer() (*http.ServeMux, *httptest.Server) {
 	stub := http.NewServeMux()
